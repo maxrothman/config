72104aa3c442aa29bc7cb6a7347dd945494b433d
Max Rothman <max@edx.org>
2017-01-11 12:38:27 -0500

    Merge changes from source-highlight package update

--- bin/src-hilite-lesspipe.sh
+++ bin/src-hilite-lesspipe.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env/bash
+#!/bin/sh
 
 #My extensions to the standard src-hilite-lesspipe.sh provided by gnu source-highlite
 # <http://www.gnu.org/software/src-highlite/>
@@ -6,8 +6,8 @@
 #Adds an environment variable SOURCE_HIGHLIGHT_BASH_EXT that defines additional specific files
 # that should be highlighted as bash in less. See .bashrc for an example.
 #
-# Install: just stick it somewhere and point LESSPIPE at it (see bashrc.d/lesspipe.sh).
-#   Don't overwrite where brew puts the default script or you'll end up fighting with it.
+#Location: /usr/local/bin/src-hilite-lesspipe.sh
+# if you installed src-highlite with brew like I did.
 
 for source in "$@"; do
   case $source in
@@ -15,6 +15,8 @@ for source in "$@"; do
       source-highlight --failsafe -f esc --lang-def=changelog.lang --style-file=esc.style -i "$source" ;;
     *Makefile|*makefile) 
       source-highlight --failsafe -f esc --lang-def=makefile.lang --style-file=esc.style -i "$source" ;;
+    *.tar|*.tgz|*.gz|*.bz2|*.xz)
+      lesspipe "$source" ;;
     *)
       if grep -q `basename $source` <<< $SOURCE_HIGHLIGHT_BASH_EXT; then
         source-highlight --failsafe -f esc --lang-def=sh.lang --style-file=esc.style -i "$source"
