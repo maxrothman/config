b9b872bb2d6669e61fff072760cbff891662eddb
Max Rothman <max-rothman@pluralsight.com>
2017-04-24 16:54:40 -0400

    Support user or globally-installed sublime

--- bashrc.d/sublime.sh
+++ bashrc.d/sublime.sh
@@ -1,3 +1,7 @@
 # Always open sublime text in a new window
-export PATH=/Users/max/Applications/Sublime\ Text.app/Contents/SharedSupport/bin:"$PATH"
+if [[ -e /Users/max/Applications/Sublime\ Text.app/Contents/SharedSupport/bin ]]; then
+  export PATH=/Users/max/Applications/Sublime\ Text.app/Contents/SharedSupport/bin:"$PATH"
+else
+  export PATH=/Applications/Sublime\ Text.app/Contents/SharedSupport/bin:"$PATH"
+fi
 alias subl='subl -n'
