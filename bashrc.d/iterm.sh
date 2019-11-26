6dd2d80f8d97715b1f7fd071f907bcdd418f0cdf
Max Rothman <max.r.rothman@gmail.com>
2019-11-26 18:36:53 -0500

    Playing with custom iterm titles

--- bashrc.d/iterm.sh
+++ bashrc.d/iterm.sh
@@ -3,3 +3,12 @@
 notify() {
   echo -ne "\e]9;${*}\007"
 }
+
+# Used in custom titles
+function iterm2_print_user_vars() {
+  local git_repo_path="$(git rev-parse --show-toplevel 2>/dev/null)"
+  local r=$? 
+  if [ $r -eq 0 ]; then
+    iterm2_set_user_var git_repo_path "$git_repo_path"
+  fi
+}
