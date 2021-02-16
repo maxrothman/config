501300c06003cc1b02df490803434c3e0d0f0c49
Max Rothman <max.rothman@reifyhealth.com>
2021-02-16 16:17:21 -0500

    Add tool for jamming AWS temp creds into env vars

--- bashrc.d/aws-mfa.sh
+++ bashrc.d/aws-mfa.sh
@@ -1,6 +1,7 @@
 # Pass-through alias to automate MFA. Useful if your account requires MFA for
 # CLI use. Put your MFA serial into ~/.aws/mfa-serial and it should just work!
-
+# This obviously won't work if you need access to multiple accounts. I also
+# currently need to assume roles as well, so this tool is currently on ice.
 
 aws_mfa () {
   local temp_creds_path="$HOME/.aws/temp-creds"
@@ -18,5 +19,32 @@ aws_mfa () {
   eval "$env_vars \aws $@"
 }
 
-alias aws=aws_mfa
+# Uncomment to enable the alias
+#alias aws=aws_mfa
 
+
+# Given a profile, prompts for an MFA token, then fetches temporary credentials
+# using the user's first MFA device and stores them in the appropriate
+# environment variables.
+
+aws-activate() {
+  if [ -z "$1" ]; then
+    echo "Usage: aws_activate PROFILE"
+    return 1
+  fi
+
+  local current_user="$(aws --profile "$1" sts get-caller-identity \
+    | jq -r '.Arn' \
+    | sed -r 's%arn:aws:iam::[0-9]+:user/(.+)%\1%')"
+  [ -z "$current_user" ] && return 1
+
+  local mfa_serial="$(aws --profile "$1" iam list-mfa-devices --user-name "$current_user" \
+    | jq -r '.MFADevices[0].SerialNumber')"
+  [ -z "$mfa_serial" ] && return 1
+
+  read -p "MFA token: " token
+  local creds="$(aws --profile "$1" sts get-session-token --serial-number "$mfa_serial" --token-code $token)"
+  export AWS_ACCESS_KEY_ID="$(    echo "$creds" | jq -r '.Credentials.AccessKeyId')"
+  export AWS_SECRET_ACCESS_KEY="$(echo "$creds" | jq -r '.Credentials.SecretAccessKey')"
+  export AWS_SESSION_TOKEN="$(    echo "$creds" | jq -r '.Credentials.SessionToken')"
+}
