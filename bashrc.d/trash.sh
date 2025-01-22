ada250ae69896cb8d7dc8aed8eaa3e55e9424585
Max Rothman <max.r.rothman@gmail.com>
2025-01-21 20:30:11 -0500

    Get trash linked properly

--- bashrc.d/trash.sh
+++ bashrc.d/trash.sh
@@ -2,6 +2,7 @@
 # Yeah, yeah, I know this is a "Bad Idea" but I've been burned too many times.
 # If you really want to delete something, you can just use `\rm`.
 
-if [ -f "$HOMEBREW_PREFIX"/bin/trash ]; then
+if [ -f "$HOMEBREW_PREFIX"/opt/trash/bin/trash ]; then
+  export PATH="$HOMEBREW_PREFIX/opt/trash/bin:$PATH"
   alias rm=trash
 fi
