79885dd6ed5c4e45480c0bc1b51d243d3140af02
Max Rothman <max.r.rothman@gmail.com>
2025-02-18 10:48:28 -0500

    Check in bettertouchtool prefs

--- deploy.sh
+++ deploy.sh
@@ -49,3 +49,5 @@ for f in "$configdir"/misc-stuff/joyride/scripts/*; do
 done
 
 deploy "$configdir"/misc-stuff/ignore ~/.config/git/ignore
+deploy "$configdir"/bettertouchtool/BetterTouchTool ~/Library/'Application Support'/BetterTouchTool
+
