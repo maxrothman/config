966fcf3417e48199830d9a19a738d9d305df3cf9
Max Rothman <max.r.rothman@gmail.com>
2025-02-18 11:22:10 -0500

    Updates for new asdf version

--- bashrc.d/asdf-packages.sh
+++ bashrc.d/asdf-packages.sh
@@ -1,8 +1,10 @@
+if [ -d "$HOMEBREW_PREFIX/opt/asdf" ]; then
+  export PATH="{ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
+  . <(asdf completion bash)
+fi
+
 # npm completion > ~/.npm/completion
 if [ -f ~/.npm/completion ]; then
   . ~/.npm/completion
 fi
 
-if [ -d "$HOMEBREW_PREFIX/opt/asdf" ]; then
-  . "$HOMEBREW_PREFIX"/opt/asdf/bin/asdf
-fi
